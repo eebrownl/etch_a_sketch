afce3e005787c0cca7013a14edef4b40c06698c2
Erika Brownlee <eebrownl@gmail.com>
2022-05-03 10:56:57 -0600

    added a reset function to reset div colors to original after etching

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 const container = document.querySelector('.container');
-//let gridSide = 80
-
-//createGrid(gridSide);
-
-//function createGrid(gridSide) {
- //   container.style.gridTemplateColumns = `repeat(${gridSide}, 1fr)`;
- //   container.style.gridTemplateRows = `repeat(${gridSide}, 1fr)`;
- //   let totalCells = gridSide ** 2
-
-
- //   for (let i = 0; i < totalCells; i++) {
-  //      let cell = document.createElement('div');
- //       container.appendChild(cell).className = 'cell';
- //   }
-//}
 
 container.addEventListener('mouseover', etch)
 
@@ -30,6 +15,9 @@ function requestSize() {
     let input = prompt('How long would you like the rows and columns to be?')
     let size = parseInt(input)
 
+    if (size > 100 || size < 1) {
+        alert('Please choose a number between 1 and 100')
+    } else {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     let totalCells = size ** 2
@@ -38,6 +26,32 @@ function requestSize() {
     for (let i = 0; i < totalCells; i++) {
         let cell = document.createElement('div');
         container.appendChild(cell).className = 'cell';
-    }
-}
+    }}}
 
+    let reset = document.querySelector('#reset');
+    reset.addEventListener('click', clearDivs);
+
+    function clearDivs() {
+        let cells = document.querySelectorAll('.cell');
+        cells.forEach(cell => {
+            cell.style.backgroundColor = 'gainsboro'
+        })
+    }
+
+//Keeping this code for the memories! I wrote this before I allowed for user input on sizing to make sure that I could make the etching fucntion work first
+
+//let gridSide = 80
+
+//createGrid(gridSide);
+
+//function createGrid(gridSide) {
+ //   container.style.gridTemplateColumns = `repeat(${gridSide}, 1fr)`;
+ //   container.style.gridTemplateRows = `repeat(${gridSide}, 1fr)`;
+ //   let totalCells = gridSide ** 2
+
+
+ //   for (let i = 0; i < totalCells; i++) {
+  //      let cell = document.createElement('div');
+ //       container.appendChild(cell).className = 'cell';
+ //   }
+//}
